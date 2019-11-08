bcdadd8551e5570b975233172a4c5009b6909077
Keelhauled <keelhauledhs@gmail.com>
2019-11-08 02:21:49 +0200

    One more range check patch

--- InfiniteAccessories/InfiniteAccessories.cs
+++ InfiniteAccessories/InfiniteAccessories.cs
@@ -6,19 +6,24 @@ using System.Text;
 using BepInEx;
 using BepInEx.Harmony;
 using HarmonyLib;
+using BepInEx.Logging;
 
 namespace InfiniteAccessories
 {
     [BepInPlugin(GUID, "Infinite Accessories", Version)]
-    public class InfiniteAccessories
+    public class InfiniteAccessories : BaseUnityPlugin
     {
         public const string GUID = "infiniteaccessories";
         public const string Version = "1.0.0";
 
+        internal new static ManualLogSource Logger;
+        internal static Harmony Harmony;
+
         private void Awake()
         {
-            var harmony = new Harmony($"{GUID}.harmony");
-            RemoveRangeChecks.Patch(harmony);
+            Logger = base.Logger;
+            Harmony = new Harmony($"{GUID}.harmony");
+            RemoveRangeChecks.Patch();
         }
     }
 }
--- InfiniteAccessories/RemoveRangeChecks.cs
+++ InfiniteAccessories/RemoveRangeChecks.cs
@@ -4,12 +4,24 @@ using System.Linq;
 using System.Reflection;
 using System.Reflection.Emit;
 using System.Text;
+using BepInEx.Harmony;
 using HarmonyLib;
 
 namespace InfiniteAccessories
 {
     internal static class RemoveRangeChecks
     {
+        public static void Patch()
+        {
+            var methodInfo_RemoveRangeCheck = AccessTools.Method(typeof(RemoveRangeChecks), nameof(RemoveRangeCheck));
+            foreach(var method in methods)
+                InfiniteAccessories.Harmony.Patch(method, null, null, new HarmonyMethod(methodInfo_RemoveRangeCheck));
+
+            var methodInfo_FindIterator = AccessTools.Method(typeof(RemoveRangeChecks), nameof(FindIterator));
+            var methodInfo_ChangeAccessoryAsync = AccessTools.Method(typeof(ChaControl), nameof(ChaControl.ChangeAccessoryAsync));
+            InfiniteAccessories.Harmony.Patch(methodInfo_ChangeAccessoryAsync, null, null, new HarmonyMethod(methodInfo_FindIterator));
+        }
+
         private static MethodInfo[] methods = new[]
         {
             AccessTools.Method(typeof(ChaControl), nameof(ChaControl.ChangeAccessoryColor)),
@@ -24,13 +36,6 @@ namespace InfiniteAccessories
             AccessTools.Method(typeof(ChaControl), nameof(ChaControl.UpdateAccessoryMoveFromInfo)),
         };
 
-        public static void Patch(Harmony harmony)
-        {
-            foreach(var method in methods)
-                harmony.Patch(method, null, null, new HarmonyMethod(MethodInfo_RemoveRangeCheck));
-        }
-
-        private static MethodInfo MethodInfo_RemoveRangeCheck = AccessTools.Method(typeof(RemoveRangeChecks), nameof(RemoveRangeCheck));
         private static IEnumerable<CodeInstruction> RemoveRangeCheck(IEnumerable<CodeInstruction> instructions)
         {
             foreach(var code in instructions)
@@ -39,6 +44,47 @@ namespace InfiniteAccessories
                 code.opcode = OpCodes.Nop;
                 code.operand = null;
                 if(breakNow) break;
+            }
+
+            return instructions;
+        }
+        
+        private static IEnumerable<CodeInstruction> FindIterator(IEnumerable<CodeInstruction> instructions)
+        {
+            foreach(var code in instructions)
+            {
+                if(code.opcode == OpCodes.Newobj && code.operand is MethodInfo methodInfo)
+                {
+                    var patch = AccessTools.Method(typeof(RemoveRangeChecks), nameof(IteratorPatch));
+                    var target = AccessTools.Method(methodInfo.DeclaringType, "MoveNext");
+                    InfiniteAccessories.Harmony.Patch(target, null, null, new HarmonyMethod(patch));
+                    break;
+                }
+
+                InfiniteAccessories.Logger.LogError($"Could not find iterator from method {nameof(ChaControl.ChangeAccessoryAsync)}");
+            }
+
+            return instructions;
+        }
+
+        private static IEnumerable<CodeInstruction> IteratorPatch(IEnumerable<CodeInstruction> instructions)
+        {
+            bool intFound = false;
+
+            foreach(var code in instructions)
+            {
+                if(code.opcode == OpCodes.Ldc_I4_S && code.operand == (object)19)
+                    intFound = true;
+
+                if(intFound)
+                {
+                    if(code.opcode == OpCodes.Brtrue)
+                    {
+                        code.opcode = OpCodes.Br;
+                        InfiniteAccessories.Logger.LogInfo($"Iterator patched in {nameof(ChaControl.ChangeAccessoryAsync)}");
+                        break;
+                    }
+                }
             }
 
             return instructions;
